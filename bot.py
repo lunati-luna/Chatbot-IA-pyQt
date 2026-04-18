ee6472ea62978df640af165a451e5a89a35bbc37
Franco Luna <Lunafranco983@gmail.com>
2026-04-18 11:35:05 -0300

    Cambios de diseño interfaz

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from PyQt6 import QtWidgets, uic
 from threading import Thread
 from ia_engine import preguntar_a_gemini
@@ -7,29 +8,7 @@ class ChatBotWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("interfaz.ui", self)
-        self.setStyleSheet("""
-    QMainWindow { background-color: #1e1e1e; }
-    QTextEdit { 
-        background-color: #252526; 
-        color: #d4d4d4; 
-        border-radius: 10px; 
-        padding: 10px;
-        font-size: 14px;
-    }
-    QLineEdit { 
-        background-color: #3c3c3c; 
-        color: white; 
-        border-radius: 5px;
-        padding: 5px;
-    }
-    QPushButton {
-        background-color: #0e639c;
-        color: white;
-        font-weight: bold;
-        border-radius: 5px;
-    }
-    QPushButton:hover { background-color: #1177bb; }
-""")
+        
         #conexiones
         self.btn_enviar.clicked.connect(self.iniciar_hilo_ia)
         self.input_mensaje.returnPressed.connect(self.iniciar_hilo_ia)
